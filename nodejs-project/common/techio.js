56e820ba8883339a29265b8aec173c0fe8ba5014
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2021-01-22 11:50:08 +0100

    amélioration du message de félicitations

--- nodejs-project/common/techio.js
+++ nodejs-project/common/techio.js
@@ -4,12 +4,12 @@ exports.printMessage = (message, channel = "Finalisation de l'exercice") =>
     : console.log(`TECHIO> message --channel "${channel}" "${message}"`);
 
 exports.congratulateStudent = () => {
-  exports.printMessage(`👌 Nickel ! Ton code valide tout ce qui était demandé !`);
+  exports.printMessage(`🏆  Bravo, ton programme à l'air de fonctionner correctement et de respecter les consignes de l'exercice !`);
   exports.printMessage(``);
   exports.printMessage(`Avant de passer à l'exercice suivant, pense à:`);
-  exports.printMessage(`1. nettoyer ton code, pour ne garder que les instructions et commentaires nécessaires à son bon fonctionnement;`);
-  exports.printMessage(`2. commenter les parties du code les moins évidentes, pour te souvenir à quoi elles servent;`);
-  exports.printMessage(`3. puis garder une copie du code dans tes notes de cours, de manière à les retrouver rapidement quand tu en auras besoin.`);
+  exports.printMessage(`🧹  nettoyer ton code, pour ne garder que les instructions et commentaires nécessaires à son bon fonctionnement;`);
+  exports.printMessage(`📝  ajouter des commentaires aux parties du code les moins évidentes, pour te souvenir à quoi elles servent;`);
+  exports.printMessage(`📒  puis à garder une copie du code dans tes notes de cours, de manière à les retrouver rapidement quand tu en auras besoin.`);
 };
 
 exports.countLines = (sourceCode) => sourceCode.split(/[\r\n]+/).length;
